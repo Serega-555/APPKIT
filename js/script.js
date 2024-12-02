08139d0f63ddf17894bff8e66421781cb06ed405
21zozo@mail.ru <Liza1525>
2024-12-02 19:36:33 +0600

    Header промежуточный этап

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// HTML
+const changeText = document.querySelector(".header__text");
+
+changeText.addEventListener("click", function() {
+    changeText.textContent = "Text has been changed!";
+});
