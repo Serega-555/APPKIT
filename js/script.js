6dd85ac6c841099e314e14f0588d7931045eb44a
21zozo@mail.ru <Liza1525>
2024-12-04 09:03:18 +0600

    Сделал scroll на js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// HTML
-const changeText = document.querySelector(".header__text");
-
-changeText.addEventListener("click", function() {
-    changeText.textContent = "Text has been changed!";
-});
